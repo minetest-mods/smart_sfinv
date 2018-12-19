a6d4438683b338a0894d74d698f251c92fa0b889
Alexander Weber <web.alexander@web.de>
2018-12-19 14:49:30 +0100

    added swipe button to the tweaks

--- smart_sfinv_tweaks/init.lua
+++ smart_sfinv_tweaks/init.lua
@@ -7,8 +7,15 @@ smart_sfinv_api.defaults.theme_inv = smart_sfinv_api.defaults.theme_inv ..
 		'tooltip[sfinv_tweaks_rotate;Rotate rows]'
 
 
+local crafting_enhance = 	'image_button[0.5,1.6;0.8,0.8;smart_sfinv_tweaks_sweep_button.png;sfinv_tweaks_sweep;]' ..
+		'tooltip[sfinv_tweaks_sweep;Sweep crafting area]'
 
 smart_sfinv_api.register_enhancement({
+	make_formspec = function(handler, player, context, content, show_inv)
+		if context.page == "sfinv:crafting" then
+			handler.formspec_after_navfs = handler.formspec_after_navfs..crafting_enhance
+		end
+	end,
 	receive_fields = function(handler, player, context, fields)
 		if fields.sfinv_tweaks_compress then
 			context.tweaks_inv = context.tweaks_inv or maininv.get(player)
@@ -17,6 +24,10 @@ smart_sfinv_api.register_enhancement({
 		if fields.sfinv_tweaks_rotate then
 			context.tweaks_inv = context.tweaks_inv or maininv.get(player)
 			context.tweaks_inv:rotate_rows()
+		end
+		if fields.sfinv_tweaks_sweep then
+			context.tweaks_inv = context.tweaks_inv or maininv.get(player)
+			context.tweaks_inv:sweep_crafting_inventory()
 		end
 	end
 })
